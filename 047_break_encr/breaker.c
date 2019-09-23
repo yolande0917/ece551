dddce75a251ad6a80d471a88dc2b6b898c834880
jx94 <jx94@duke.edu>
2019-09-23 14:38:16 -0400

    added breaker attempt 4

--- 047_break_encr/breaker.c
+++ 047_break_encr/breaker.c
@@ -3,22 +3,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-// Find size of chars in file
-size_t sizeOfChars(FILE * f) {
-  int c;
-  size_t n = 0;
-  while ((c = fgetc(f)) != EOF) {
-    if (isalpha(c)) {
-      n++;
-    }
-  }
-  if (n == 0) {
-    fprintf(stderr, "There is no char in the input file.\n");
-    exit(EXIT_FAILURE);
-  }
-  return n;
-}
-
 int findMax(int * array, size_t n) {
   int max = *array;
   int ind = 0;
@@ -31,32 +15,71 @@ int findMax(int * array, size_t n) {
   return ind;
 }
 
-int findMaxFreqIndex(FILE * f, char * chars, int * freq, size_t n) {
+int findMaxFreq(FILE * f, const char * alpha, int * freq) {
+  int n = 26;
   int c;
-  int i = 0;
   while ((c = fgetc(f)) != EOF) {
     if (isalpha(c)) {
-      *(chars + i) = tolower(c);
-      i++;
+      c = tolower(c);  // to lower case
+
+      // compare with alpha and fill in freq
+      for (int i = 0; i < n; i++) {
+        if (c == *(alpha + i)) {          // find c in alpha
+          *(freq + i) = *(freq + i) + 1;  // increase its freq
+        }
+      }
     }
   }
 
-  int count = 0;
-  char current;
-  for (size_t j = 0; j < n; j++) {
-    current = *(chars + j);
-    count = 0;
-    for (size_t k = 0; k <= j; k++) {
-      if (*(chars + k) == current) {
-        count++;
-      }
-    }
-    *(freq + j) = count;
-  }
-  // find max index
-  int ind = findMax(freq, n);
+  int ind = findMax(freq, 26);
   return ind;
 }
+
+/* /\********************** */
+/* // Find size of chars in file */
+/* size_t sizeOfChars(FILE * f) { */
+/*   int c; */
+/*   size_t n = 0; */
+/*   while ((c = fgetc(f)) != EOF) { */
+/*     if (isalpha(c)) { */
+/*       n++; */
+/*     } */
+/*   } */
+/*   if (n == 0) { */
+/*     fprintf(stderr, "There is no char in the input file.\n"); */
+/*     exit(EXIT_FAILURE); */
+/*   } */
+/*   return n; */
+/* } */
+
+/* int findMaxFreqIndex(FILE * f, char * chars, int * freq, size_t n) { */
+/*   int c; */
+/*   int i = 0; */
+/*   while ((c = fgetc(f)) != EOF) { */
+/*     if (isalpha(c)) { */
+/*       *(chars + i) = tolower(c); */
+/*       i++; */
+/*     } */
+/*   } */
+
+/*   int count = 0; */
+/*   char current; */
+/*   for (size_t j = 0; j < n; j++) { */
+/*     current = *(chars + j); */
+/*     count = 0; */
+/*     for (size_t k = 0; k <= j; k++) { */
+/*       if (*(chars + k) == current) { */
+/*         count++; */
+/*       } */
+/*     } */
+/*     *(freq + j) = count; */
+/*   } */
+/*   // find max index */
+/*   int ind = findMax(freq, n); */
+/*   return ind; */
+/* } */
+
+/* ****\/ */
 
 int main(int argc, char ** argv) {
   if (argc != 2) {
@@ -70,6 +93,23 @@ int main(int argc, char ** argv) {
     return EXIT_FAILURE;
   }
 
+  const char * alpha = "abcdefghijklmnopqrstuvwxyz";
+  int freq[26] = {0};
+
+  int ind = findMaxFreq(f, alpha, freq);
+  if (fclose(f) != 0) {
+    fprintf(stderr, "Failed to close the input file.\n");
+    return EXIT_FAILURE;
+  }
+
+  char c = alpha[ind];
+
+  // calculate key
+  int key = ((c - 'e') + 26) % 26;
+
+  printf("The key is %d.\n", key);
+
+  /**
   size_t n = sizeOfChars(f);
   if (fclose(f) != 0) {
     fprintf(stderr, "Failed to close the input file.\n");
@@ -96,4 +136,5 @@ int main(int argc, char ** argv) {
   int key = ((c - 'e') + 26) % 26;
 
   printf("The key is %d.\n", key);
+  **/
 }
