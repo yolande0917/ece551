b732cf10517e650a20caa050d8e4ddf6cd26874a
jx94 <jx94@duke.edu>
2019-09-30 22:37:03 -0400

    added outname.c attempt 1

--- 057_outname/outname.c
+++ 057_outname/outname.c
@@ -6,4 +6,18 @@
 
 char * computeOutputFileName(const char * inputName) {
   //WRITE ME
+
+  if (inputName == NULL) {
+    fprintf(stderr, "Failed to access the input name.\n");
+    exit(EXIT_FAILURE);
+  }
+  // .counts has 7 chars
+  size_t length = strlen(inputName) + 1 + 7;
+  // malloc a new string
+  char * outName = malloc(length * sizeof(outName));
+  // copy
+  outName = strcpy(outName, inputName);
+  // append .counts
+  outName = strcat(outName, ".counts");
+  return outName;
 }
