8cdd2d652f06cccee98619d21bb8a6ce996d94e6
jx94 <jx94@duke.edu>
2019-10-09 23:06:04 -0400

    added vector2d attempt 1

--- 064_vector_2d/vector.cpp
+++ 064_vector_2d/vector.cpp
@@ -5,3 +5,26 @@
 
 /* write your class implementation in this file
  */
+double Vector2D::getMagnitude() const {
+  return std::sqrt(x * x + y * y);
+}
+
+Vector2D Vector2D::operator+(const Vector2D & rhs) const {
+  Vector2D myVector;
+  myVector.initVector(rhs.x + x, rhs.y + y);
+  return myVector;
+}
+
+Vector2D & Vector2D::operator+=(const Vector2D & rhs) {
+  x += rhs.x;
+  y += rhs.y;
+  return *this;
+}
+
+double Vector2D::dot(const Vector2D & rhs) const {
+  return x * rhs.x + y * rhs.y;
+}
+
+void Vector2D::print() const {
+  std::printf("<%.2f, %.2f>", x, y);
+}
