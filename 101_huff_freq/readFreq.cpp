015d6931a9cf316b1bfe0477d62b3a8e886a8c48
jx94 <jx94@duke.edu>
2019-11-11 15:00:56 -0500

    attempt 1

--- 101_huff_freq/readFreq.cpp
+++ 101_huff_freq/readFreq.cpp
@@ -1,9 +1,12 @@
-#include <stdio.h>
-#include <cstdlib>
 #include "readFreq.h"
 
+#include <stdio.h>
+
+#include <cstdlib>
+#include <fstream>
+
 void printSym(std::ostream & s, unsigned sym) {
-  if (sym > 256 ) {
+  if (sym > 256) {
     s << "INV";
   }
   else if (sym == 256) {
@@ -21,4 +24,23 @@ void printSym(std::ostream & s, unsigned sym) {
 }
 uint64_t * readFrequencies(const char * fname) {
   //WRITE ME!
+  std::ifstream f;
+  f.open(fname);
+  if (!f.good()) {
+    std::cerr << "Can't open the file.\n";
+    exit(EXIT_FAILURE);
+  }
+
+  uint64_t * freq = new uint64_t[257];
+  for (int i = 0; i < 257; i++) {
+    freq[i] = 0;
+  }
+  int c;
+  while ((c = f.get()) >= 0) {
+    freq[c] += 1;
+  }
+  freq[256] += 1;
+
+  f.close();
+  return freq;
 }
