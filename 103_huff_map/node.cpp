3d309cc67bb904f33f234aa7c384e99cb98ff8a1
jx94 <jx94@duke.edu>
2019-11-12 22:22:49 -0500

    attempt 1

--- 103_huff_map/node.cpp
+++ 103_huff_map/node.cpp
@@ -1,5 +1,21 @@
 #include "node.h"
-void Node::buildMap(BitString b, std::map<unsigned,BitString> & theMap) {
-  //WRITE ME!
+
+#include <cassert>
+
+void buildMapHelper(Node * node, BitString b, std::map<unsigned, BitString> & theMap) {
+  if (node->sym != NO_SYM) {
+    assert(node->left == NULL && node->right == NULL);
+    theMap.insert(std::pair<int, BitString>(node->sym, b));
+  }
+  else {
+    BitString bl = b.plusZero();
+    buildMapHelper(node->left, bl, theMap);
+    BitString br = b.plusOne();
+    buildMapHelper(node->right, br, theMap);
+  }
 }
 
+void Node::buildMap(BitString b, std::map<unsigned, BitString> & theMap) {
+  //WRITE ME!
+  buildMapHelper(this, b, theMap);
+}
